67824d956d4b72c08169ad64ccfe5a8d11e9f16b
Sander Ronde <awsdfgvhbjn@gmail.com>
2021-03-30 23:28:08 +0200

    Initial commit

---
+++ lib/net/src/net.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <string.h>
+
+#include <ESP***REMOVED***WiFi.h>
+#include <ESP***REMOVED***WiFiMulti.h>
+#include <ESP***REMOVED***HTTPClient.h>
+#include <WiFiClient.h>
+
+using namespace std;
+
+namespace Net {
+	String req(const char* host, int port, const char* path);
+
+	String req_auth(const char* host, int port, const char* path);
+
+	void setup();
+
+	void await_wifi();
+}

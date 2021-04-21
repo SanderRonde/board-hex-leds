93b8771e6c4ff88bb6eeba09ef29062800686d83
Sander Ronde <awsdfgvhbjn@gmail.com>
2021-04-22 00:58:42 +0200

    update num LEDs

--- include/leds.h
+++ include/leds.h
@@ -5,11 +5,7 @@
 
 // We say D1 but in actuality the magic happens on pin 5
 #define LED_PIN D1
-#define LEDS_PER_M 60
-#define METERS_PER_STRIP 5
-#define LEDS_PER_STRIP (LEDS_PER_M * METERS_PER_STRIP)
-#define STRIPS 1
-#define NUM_LEDS (LEDS_PER_STRIP * STRIPS)
+#define NUM_LEDS 275
 
 namespace Leds {
 	extern CRGB leds[NUM_LEDS];

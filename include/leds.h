fd8db05f92eba6960773ab176997e8fdc39a857a
Sander Ronde <awsdfgvhbjn@gmail.com>
2022-09-09 19:56:31 +0200

    change pin

--- include/leds.h
+++ include/leds.h
@@ -3,8 +3,8 @@
 #include <Arduino.h>
 #include <FastLED.h>
 
-// We say D1 but in actuality the magic happens on pin 5
-#define LED_PIN D1
+// For me: on desktop this should be D1, on server D5, don't ask me why
+#define LED_PIN D5
 #define NUM_LEDS 275
 
 namespace Leds {

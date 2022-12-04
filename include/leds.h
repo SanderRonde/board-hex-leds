578bc0af3572166883931354a8da9d2137108a14
Sander Ronde <awsdfgvhbjn@gmail.com>
2022-12-04 20:47:24 +0100

    update number of pixels

--- include/leds.h
+++ include/leds.h
@@ -5,8 +5,10 @@
 
 // We say D1 but in actuality the magic happens on pin 5
 #define LED_PIN D1
+// We have 500 in reality but don't use the last 10.
+// To prevent stuck pixels we just act like they don't exist.
+#define NUM_LEDS 490
 // #define NUM_LEDS 275
-#define NUM_LEDS 500
 
 namespace Leds {
 	extern CRGB leds[NUM_LEDS];

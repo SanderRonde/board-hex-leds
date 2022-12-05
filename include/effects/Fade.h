cba835dbf0100e5aef869b6c9eaa6496d15bec83
Sander Ronde <awsdfgvhbjn@gmail.com>
2022-12-05 11:45:44 +0100

    support circular fade effect

--- include/effects/Fade.h
+++ include/effects/Fade.h
@@ -12,16 +12,18 @@ enum FADE_DIRECTION
 	FADE_RIGHT,
 	FADE_LEFT,
 	FADE_TOP,
-	FADE_BOT
+	FADE_BOT,
+	FADE_CIRCULAR,
 };
 
 class Fade : public EffectBase
 {
-private:
+protected:
 	change_t _from_color = {0};
 	change_t _to_color = {0};
 	FADE_DIRECTION _direction;
 	bool _rendered = false;
+	float distances_from_center[NUM_LEDS] = {0};
 
 	bool _animate_from;
 	bool _animate_to;
@@ -31,9 +33,22 @@ private:
 	int _to_interval_max;
 
 	unsigned long _last_iteration;
+	void calculate_distances_from_center(Hexes *hexes);
 
 public:
-	Fade(Hexes* hexes, RequestObj *request);
+	Fade(Hexes* hexes);
 	std::map<int, float> calculate_pos_map(Hexes* hexes);
 	bool loop(Hexes *hexes) override;
+};
+
+class FadeFromReq : public Fade
+{
+public:
+	FadeFromReq(Hexes* hexes, RequestObj *request);
+};
+
+class FadeManual : public Fade
+{
+public:
+	FadeManual(Hexes* hexes);
 };
--- src/effects/Fade.cpp
+++ src/effects/Fade.cpp
@@ -21,10 +21,46 @@ FADE_DIRECTION parse_direction_str(String direction_str)
 	{
 		return FADE_BOT;
 	}
+	else if (direction_str == "circular")
+	{
+		return FADE_CIRCULAR;
+	}
 	throw std::exception();
 }
 
-Fade::Fade(Hexes *hexes, RequestObj *request) : EffectBase()
+void Fade::calculate_distances_from_center(Hexes *hexes)
+{
+	float distances[NUM_LEDS];
+	float min_distance = 1.0f;
+	float max_distance = 0.0f;
+
+	for (int i = 0; i < NUM_LEDS; i++)
+	{
+		float led_x_pos = hexes->get_led_x_pos_for_index(i) - 0.5f;
+		float led_y_pos = hexes->get_led_y_pos_for_index(i) - 0.5f;
+
+		// Distance from center is just pythagoras' theorem
+		float distance_from_center = sqrt(pow(led_x_pos, 2) + pow(led_y_pos, 2));
+		distances[i] = distance_from_center;
+
+		min_distance = min(min_distance, distance_from_center);
+		max_distance = max(max_distance, distance_from_center);
+	}
+
+	for (int i = 0; i < NUM_LEDS; i++)
+	{
+		distances_from_center[i] = (distances[i] - min_distance) / (max_distance - min_distance);
+	}
+}
+
+Fade::Fade(Hexes *hexes) : EffectBase()
+{
+
+	calculate_distances_from_center(hexes);
+	_last_iteration = millis();
+}
+
+FadeFromReq::FadeFromReq(Hexes *hexes, RequestObj *request) : Fade(hexes)
 {
 	_direction = parse_direction_str(request->stringv("direction"));
 	_animate_from = request->has("animate_from") ? request->boolv("animate_from") : false;
@@ -51,8 +87,21 @@ Fade::Fade(Hexes *hexes, RequestObj *request) : EffectBase()
 	{
 		_to_color = Change::create_locked(request->colorv("to"));
 	}
+}
 
-	_last_iteration = millis();
+FadeManual::FadeManual(Hexes *hexes) : Fade(hexes)
+{
+	_direction = FADE_RIGHT;
+	_animate_from = true;
+	_animate_to = true;
+
+	_from_interval_min = 10000;
+	_from_interval_max = 20000;
+	_from_color = Change::set_new_color_target(_from_color, _from_interval_min, _from_interval_max);
+
+	_to_interval_min = 10000;
+	_to_interval_max = 20000;
+	_to_color = Change::set_new_color_target(_to_color, _to_interval_min, _to_interval_max);
 }
 
 bool Fade::loop(Hexes *hexes)
@@ -82,6 +131,7 @@ bool Fade::loop(Hexes *hexes)
 		}
 	}
 
+	bool is_circular = _direction == FADE_CIRCULAR;
 	bool is_x = _direction == FADE_LEFT || _direction == FADE_RIGHT;
 	bool invert = _direction == FADE_BOT || _direction == FADE_LEFT;
 	auto from_color = Change::get_current_color(_from_color);
@@ -92,7 +142,19 @@ bool Fade::loop(Hexes *hexes)
 		auto hex = hexes->get_by_index(i);
 		for (size_t j = 0; j < hex->num_leds; j++)
 		{
-			float led_relative_pos = is_x ? hex->get_led_global_x_position_at_index(j) : hex->get_led_global_y_position_at_index(j);
+			float led_relative_pos;
+			if (is_circular)
+			{
+				led_relative_pos = distances_from_center[hex->get_at_index(j)];
+			}
+			else if (is_x)
+			{
+				led_relative_pos = hex->get_led_global_x_position_at_index(j);
+			}
+			else
+			{
+				led_relative_pos = hex->get_led_global_y_position_at_index(j);
+			}
 
 			if (invert)
 			{

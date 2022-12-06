8301e4b0f5625cd13d388f6c1ccb3fd0287b6605
Sander Ronde <awsdfgvhbjn@gmail.com>
2022-12-06 22:39:41 +0100

    also works

--- include/config.h
+++ include/config.h
@@ -2,3 +2,16 @@
 
 // #define NAME "board-hex-leds"
 #define NAME "board-hex-desk-leds"
+
+#define USED_LED_CONFIG desk
+
+#define NUM_LEDS_bed 275
+// We have 500 in reality but don't use the last 10.
+// To prevent stuck pixels we just act like they don't exist.
+#define NUM_LEDS_desk 489
+// I'm sure this can be done in a better way
+#if USED_LED_CONFIG == desk
+#define NUM_LEDS2 NUM_LEDS_desk
+#elif USED_LED_CONFIG == bed
+#define NUM_LEDS2 NUM_LEDS_bed
+#endif

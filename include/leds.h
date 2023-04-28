27d08b7297076affdb78f397af35a756e4ecc1f5
Sander Ronde <awsdfgvhbjn@gmail.com>
2023-04-29 01:53:13 +0200

    add branch for circular hea

--- include/leds.h
+++ include/leds.h
@@ -4,8 +4,10 @@
 #include <FastLED.h>
 
 // For me: on desktop this should be D1, on server D5, don't ask me why
-#define LED_PIN D5
-#define NUM_LEDS 275
+#define LED_PIN D1
+// We have 500 in reality but don't use the last 10.
+// To prevent stuck pixels we just act like they don't exist.
+#define NUM_LEDS 489
 
 namespace Leds {
 	extern CRGB leds[NUM_LEDS];
